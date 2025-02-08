93ba0a503694ac67eb4994ff16d2f81a16286f1f
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-02-09 00:34:16 +0900

    feat: file arrangement

--- react/prerender/index.ts
+++
@@ -1,7 +0,0 @@
-const React = require("react");
-const ReactDOMServer = require("react-dom/server");
-const App = require("../src/page/App.tsx").default;
-
-const prerenderedHtml = ReactDOMServer.renderToString(React.createElement(App));
-
-module.exports = prerenderedHtml;
